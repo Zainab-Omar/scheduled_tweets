df31685a7c520e5add018847495f3c72ec204bb2
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-09-23 18:45:23 -0500

    create destroy method to log user out

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,7 @@ class RegistrationsController < ApplicationController
     end
 
     def create
+        # create new user "signup form"
         @user = User.new(user_params)
         if @user.save
             redirect_to root_path, notice: "Successfully created an account"
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,7 @@
 class SessionsController < ApplicationController
-    
+    def destroy
+        session.delete :user_id
+        redirect_to root_path
+        flash[:notice] = "Successfully logged out"
+    end
 end
