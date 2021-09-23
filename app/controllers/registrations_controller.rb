a6fe17bbd02942ccb310a13cec528078f58dc1a9
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-09-23 17:21:53 -0500

    add changes to sign_up form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,7 +6,7 @@ class RegistrationsController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            redirect_to root_path, notice: "Successfully create an account"
+            redirect_to root_path, notice: "Successfully created an account"
         else
             render :new
         end
