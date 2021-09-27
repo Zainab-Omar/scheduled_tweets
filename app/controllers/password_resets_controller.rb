02a46f2390949c85914833522fcc2d01aed8afc5
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-09-27 16:27:02 -0500

    create password mailer

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,7 +1,14 @@
 class PasswordResetsController < ApplicationController
     def new
     end
-    
+
     def create 
+        @user = User.find_by(email: params[:email])
+        if @user.present?
+            #send email
+            PasswordMailer.with(user: @user).reset.deliver_now
+            redirect_to root_path, notice: "Reset link sent to your email"
+        else
+            redirect_to root_path, notice: "Invalid email"
     end
 end
