a2c30f1f20acbb99d8bb256684f1d9a3bd4c2abc
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-09-30 19:00:39 -0500

    build update method for password_reset_controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -20,4 +20,20 @@ class PasswordResetsController < ApplicationController
         redirect_to sign_in_path, alert: "Your token has expired try again"
     end
 
+    def update
+        @user = User.find_signed(params[:token], purpose: "password_reset")
+        if @user.update(password_params)
+            redirect_to sign_in_path, notice: "password updated successfully please sign in"
+        else
+            render :edit
+        end    
+    end
+
+    private
+
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
+    end
+
+
 end
